8bd85a4e6a6ed7e79bdee80eec0758aa67c4b705
unknown <sonwj0915@naver.com>
2021-04-10 23:02:54 +0900

    최종 리팩토링 완료

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -21,6 +21,35 @@ export const createPromiseThunk = (type, promiseCreator) => {
   return thunkCreator;
 };
 
+// type은 createPromiseThunk의 타입이랑 동일
+// 각 action들 마다 사용하는 key가 다름(post, posts, ...등등)
+export const handleAsyncActions = (type, key) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  // reducer 임
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: reducerUtils.loading(),
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: reducerUtils.success(),
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: reducerUtils.error(),
+        };
+      default:
+        return state;
+    }
+  };
+};
+
 export const reducerUtils = {
   initial: (data = null) => ({
     data,
